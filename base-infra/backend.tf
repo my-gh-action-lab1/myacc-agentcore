36d47ed06debb459aa3657d094d1551177147e1c
mdanish0595 <mdanish0595@gmail.com>
2026-07-20 20:52:25 +0100

    Update backend.tf

--- base-infra/backend.tf
+++ base-infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "dan-terraform-01"
+    bucket  = "dan-terraform-101"
     key     = "myacc-agentcore/base-infra/terraform.tfstate"
     region  = "us-west-2"
     encrypt = true
